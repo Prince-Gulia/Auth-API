27101f6eba26726589d2c1feeebb669f0597374c
Prince Gulia <princegulia170306@gmail.com>
2026-05-17 10:38:46 +0530

    add db connection log

--- config/db.js
+++ config/db.js
@@ -6,4 +6,13 @@ const pool = new Pool({
     ssl : { rejectUnauthorized: false }
 });
 
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('Database connection error:', err.message);
+  } else {
+    console.log('Database connected successfully');
+    release();
+  }
+});
+
 module.exports = pool;
